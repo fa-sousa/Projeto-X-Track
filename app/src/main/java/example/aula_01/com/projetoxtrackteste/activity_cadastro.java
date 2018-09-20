a9e2e2ad8bd8202537e72ed6e53754af8bb48280
fa-sousa <fsousa292@gmail.com>
2018-09-20 20:35:36 -0300

    inclusao do json na pasta app

--- app/src/main/java/example/aula_01/com/projetoxtrackteste/activity_cadastro.java
+++ app/src/main/java/example/aula_01/com/projetoxtrackteste/activity_cadastro.java
@@ -2,6 +2,9 @@ package example.aula_01.com.projetoxtrackteste;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class activity_cadastro extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class activity_cadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        iniciaBusca(); //este método faz a consulta ao XML e retorna os dados para serem exibidos na listView acima
+        }
     }
 }
