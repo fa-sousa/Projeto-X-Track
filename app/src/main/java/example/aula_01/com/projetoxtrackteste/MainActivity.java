0a7a00960539134d7b33d614c09816a8fdf2e95d
wmtbr <willian.arq@terra.com.br>
2018-09-19 21:34:49 -0300

    commit willian

---
+++ app/src/main/java/example/aula_01/com/projetoxtrackteste/MainActivity.java
@@ -0,0 +1,14 @@
+package example.aula_01.com.projetoxtrackteste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //teste
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
