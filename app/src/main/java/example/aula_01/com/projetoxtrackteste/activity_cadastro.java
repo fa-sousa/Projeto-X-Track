e35329a25364ba602f4c0c02f885befdf5251be1
fa-sousa <fsousa292@gmail.com>
2018-09-20 21:03:42 -0300

    alteracao botoes tela cadastro

--- app/src/main/java/example/aula_01/com/projetoxtrackteste/activity_cadastro.java
+++ app/src/main/java/example/aula_01/com/projetoxtrackteste/activity_cadastro.java
@@ -1,19 +1,38 @@
 package example.aula_01.com.projetoxtrackteste;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class activity_cadastro extends AppCompatActivity {
+
+    Button botaoCancelar = (Button) findViewById(R.id.botaoCancelar);
+    Button botaoConfirmar = (Button) findViewById(R.id.botaoConfirmar);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
-        iniciaBusca(); //este método faz a consulta ao XML e retorna os dados para serem exibidos na listView acima
-        }
+        botaoCancelar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(activity_cadastro.this, principal.class);
+                startActivity(it);
+            }
+        });
+
+        botaoConfirmar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(activity_cadastro.this, principal.class);
+                startActivity(it);
+            }
+        });
+
     }
 }
